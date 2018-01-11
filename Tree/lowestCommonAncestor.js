5014ae958420895a3c89955cd9328a2c2a62d175
Sohini Makar <sohini.makar@gmail.com>
2018-01-11 15:48:58 -0800

    lowest (nearest) common ancestor in BST

---
+++ Tree/lowestCommonAncestor.js
@@ -0,0 +1,42 @@
+//Given the value of two nodes in a binary search tree, find the lowest (nearest) common ancestor. You mayassume that both values already exist in the tree.
+class Tree{
+  constructor(val){
+    this.val = val
+    this.left = null
+    this.right = null
+  }
+  insertLeft(val){
+    this.left = new Tree(val)
+    return this.left
+  }
+  
+  insertRight(val){
+    this.right = new Tree(val)
+    return this.right
+  }
+}
+
+var root = new Tree(4)
+var left = root.insertLeft(2)
+var right = root.insertRight(6)
+left.insertLeft(1)
+left.insertRight(3)
+right.insertLeft(5)
+right.insertRight(7)
+
+function lowestCommonAncestor(root,v1,v2){
+  if(!root) return 'root can not be null'
+  while(root){
+    if(root.val > v1 && root.val > v2){
+      root = root.left
+    }else if(root.val < v1 && root.val < v2){
+      root = root.right
+    }else{
+      return root
+    }
+  }
+  return null
+}
+
+//------ test --------
+lowestCommonAncestor(root,7,5) // node with val 6
