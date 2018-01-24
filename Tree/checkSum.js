c27a0f7a6ed74229238a93a95cc86d2d9b53b46b
Sohini Makar <sohini.makar@gmail.com>
2018-01-24 12:13:57 -0800

    two nodes a  and b such that a+b=k
    Given a Binary Search Tree with integers at every node and an integer k, write code that decides whether or not there exists two nodes a and b such that a+b=k.

--- Tree/checkSum.js
+++ Tree/checkSum.js
@@ -20,13 +20,13 @@ class Tree{
 }
 
 var root1 = new Tree(4)
-var left1 = root1.insertLeft(20)
+var left1 = root1.insertLeft(2)
 var right1 = root1.insertRight(6)
 left1.insertLeft(1)
 left1.insertRight(3)
 right1.insertLeft(5)
 right1.insertRight(7)
-
+//-------------------------------
 
 function buildArr(root, arr){
   if(!root) return
@@ -47,5 +47,27 @@ function checkSum(root, k){
   return false
 }
 
+//------ another way ---------
+function search(root, val, node){
+  if(!root) return false
+  if(root.val == val && root != node) return true
+  /*
+  if(val < root.val){
+    return search(root.left, val, node)
+  }
+  else if(val > root.val){
+    return search(root.right, val, node)
+  }
+  return false*/
+  return search((val < root.val) ? root.left : root.right, val, node)
+}
+
+function checkSum(root,current,k){
+  if (!current) return false
+  if (search(root, k - current.val, current)) return true
+  return (checkSum(root, current.left, k) || checkSum(root, current.right, k))
+}
+
+//------ test --------
 //checkSum(root1, 2) //false
 checkSum(root1, 4) //true
