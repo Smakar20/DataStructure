75b3c449431abb0d0b78398ef85a14e5416b9cd5
Sohini Makar <sohini.makar@gmail.com>
2022-05-27 15:14:20 -0700

    Path Sum

--- Tree/hasPathSum.js
+++ Tree/hasPathSum.js
@@ -1,8 +1,8 @@
-/*Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the 
-path equals the given sum.
+/*Given a binary tree and a targetSum, determine if the tree has a root-to-leaf path such that adding up all the values along the 
+path equals the given targetSum.
 
 For example:
-Given the below binary tree and sum = 22,
+Given the below binary tree and targetSum = 22,
 
               5
              / \
@@ -11,11 +11,10 @@ Given the below binary tree and sum = 22,
           11  13  4
          /  \      \
         7    2      1
-return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.*/
+return true, as there exist a root-to-leaf path 5->4->11->2 which targetSum is 22.*/
 
-var hasPathSum = function(root, sum) {
-    if(!root) return false
-    if(!root.left && !root.right && sum - root.val == 0) return true
-    return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val)
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    if (!root.right && !root.left && root.val === targetSum) return true;
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 };
-
