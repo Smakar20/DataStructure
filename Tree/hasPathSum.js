3f224a22313aa6c0f92c53618f0e725218df2c6d
Sohini Makar <sohini.makar@gmail.com>
2022-05-27 15:18:09 -0700

    Path sum

--- Tree/hasPathSum.js
+++ Tree/hasPathSum.js
@@ -1,4 +1,5 @@
-/*Given a binary tree and a targetSum, determine if the tree has a root-to-leaf path such that adding up all the values along the 
+/*
+Given a binary tree and a targetSum, determine if the tree has a root-to-leaf path such that adding up all the values along the 
 path equals the given targetSum.
 
 For example:
@@ -11,8 +12,14 @@ Given the below binary tree and targetSum = 22,
           11  13  4
          /  \      \
         7    2      1
-return true, as there exist a root-to-leaf path 5->4->11->2 which targetSum is 22.*/
-
+return true, as there exist a root-to-leaf path 5->4->11->2 which targetSum is 22.
+*/
+//Time complexity: the same as the recursion approach \mathcal{O}(N)O(N).
+/*
+Space complexity: mathcal{O}(N)O(N) since in the worst case, when the tree is completely unbalanced, e.g. each node has only one child node, we would keep 
+all NN nodes in the stack. But in the best case (the tree is balanced), the height of the tree would be \log(N)log(N). Therefore, the space complexity in 
+this case would be \mathcal{O}(\log(N))O(log(N)).
+*/
 var hasPathSum = function(root, targetSum) {
     if (!root) return false;
     if (!root.right && !root.left && root.val === targetSum) return true;
