3a0b8fbe17269575eed1d450dd09dd9a69171e9e
Sohini Makar <sohini.makar@gmail.com>
2022-05-27 15:19:10 -0700

    Path sun

--- Tree/hasPathSum.js
+++ Tree/hasPathSum.js
@@ -14,11 +14,11 @@ Given the below binary tree and targetSum = 22,
         7    2      1
 return true, as there exist a root-to-leaf path 5->4->11->2 which targetSum is 22.
 */
-//Time complexity: the same as the recursion approach \mathcal{O}(N)O(N).
+//Time complexity: O(N).
 /*
-Space complexity: mathcal{O}(N)O(N) since in the worst case, when the tree is completely unbalanced, e.g. each node has only one child node, we would keep 
-all NN nodes in the stack. But in the best case (the tree is balanced), the height of the tree would be \log(N)log(N). Therefore, the space complexity in 
-this case would be \mathcal{O}(\log(N))O(log(N)).
+Space complexity: O(N) since in the worst case, when the tree is completely unbalanced, e.g. each node has only one child node, we would keep 
+all N nodes in the stack. But in the best case (the tree is balanced), the height of the tree would be \log(N)log(N). Therefore, the space complexity in 
+this case would be O(log(N)).
 */
 var hasPathSum = function(root, targetSum) {
     if (!root) return false;
