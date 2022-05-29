74edaf0d1df236c1d712346dfaffe26947456525
Sohini Makar <sohini.makar@gmail.com>
2022-05-29 13:33:41 -0700

    Populating Next Right Pointers in Each Node II

---
+++ Tree/connect2.js
@@ -0,0 +1,58 @@
+/*
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL. 
+
+Example 1:
+
+                1 -> null
+            /       \
+          2      ->    3 -> null
+         /  \           \
+        4 ->  5  ->       7 -> null
+                
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+Example 2:
+Input: root = []
+Output: []
+ 
+Constraints:
+The number of nodes in the tree is in the range [0, 6000].
+-100 <= Node.val <= 100
+ 
+
+Follow-up:
+
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+*/
+
+var connect = function(root) {
+    if (!root) return null;
+    if (root.left) root.left.next = root.right ? root.right : findChild(root.next);
+    if (root.right) root.right.next = findChild(root.next);
+    connect(root.right);
+    connect(root.left);
+    return root;
+};
+
+function findChild(root) {
+    let cur = root;
+    while (cur) {
+        if (cur.left) return cur.left;
+        if (cur.right) return cur.right;
+        cur = cur.next;
+    }
+    return null;
+}
